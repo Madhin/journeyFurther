6b94713ec9d94cf17c2af744f9507dadd03e1ae2
Madhin <madhinhussain@gmail.com>
2024-05-09 22:58:09 +0100

    waitForElem function added

--- src/indexGHD.js
+++ src/indexGHD.js
@@ -5,7 +5,7 @@ import css from './assets/scss/styleTest2.scss'
 // Get a helper to make our style element
 
 import {
-  // waitForElm,
+  waitForElm,
   // isMobile,
   retryTest,
   handleLoadFailure,
@@ -18,9 +18,7 @@ const styleElementString = makeStyleElementString(css.toString())
 // IIFE to initialise the test and catch any errors
 ;(function init(tries = 0) {
   try {
-    // console.log('should run')
-    // If this is an iFrame exit -- prevents our tests from appearing in iFrames
-    // if (window.location.href.includes('?sidebar=true')) return
+
     // if we have tried to load more than X times we failed
     if (tries > 5) return handleLoadFailure()
     // Add some logic to check for a target here and then retry
@@ -39,50 +37,60 @@ const styleElementString = makeStyleElementString(css.toString())
 
   ;(function () {
 
-    //function that hides bad reviews and highest and lowest rating select options
-    const reviewHandler = () => {
+
+    // window.addEventListener('scroll', function() {
+    //   // Your code to execute when scrolling occurs
+    //   console.log('Page is being scrolled');
+      waitForElm('.has-reviews ')
+    .then(() => {
+      console.log('Element is ready')
     
-    // all reviews containers 
-     const reviewRating = document.querySelectorAll('.wrapper-reviews .row.mb-3.review-wrapper')
+    }).then(()=>{  
+      //function that hides bad reviews and highest and lowest rating select options
+      const reviewHandler = () => {
+      
+        // all reviews containers 
+         const reviewRating = document.querySelectorAll('.wrapper-reviews .row.mb-3.review-wrapper')
+        
+         // hide reviews function
+         const hideReviewsHandler =  () => reviewRating.forEach((el)=> {
+          const reviewNumber = Number(el.querySelector('.total').textContent.trim().charAt(0))
+          // console.log(reviewNumber)
+          if (reviewNumber <=3 ) {
+            el.style.display = 'none'
+          }
+         })
     
-     // hide reviews function
-     const hideReviewsHandler =  () => reviewRating.forEach((el)=> {
-      const reviewNumber = Number(el.querySelector('.total').textContent.trim().charAt(0))
-      console.log(reviewNumber)
-      if (reviewNumber <=3 ) {
-        el.style.display = 'none'
-      }
-     })
-
-     hideReviewsHandler()
-
-    // function that hides the higest and lowest rating options 
-     const hideRatings = () => {
-     const highestRating = document.querySelector('.has-reviews option[value="highestRating"]')
-     const lowestRating = document.querySelector('.has-reviews option[value="lowestRating"]')
-     highestRating.style.display = 'none'
-     lowestRating.style.display = 'none'
-     }
-     hideRatings()
-    }
-
-    reviewHandler()
-
-    const reviewContainer = document.querySelector('.wrapper-reviews')
-
-      // Mutationobserver to observe changes to page
-      const observer = new MutationObserver(() => {
-        // console.log('mutation')
+         hideReviewsHandler()
+    
+        // function that hides the higest and lowest rating options 
+         const hideRatings = () => {
+         const highestRating = document.querySelector('.has-reviews option[value="highestRating"]')
+         const lowestRating = document.querySelector('.has-reviews option[value="lowestRating"]')
+         highestRating.style.display = 'none'
+         lowestRating.style.display = 'none'
+         }
+         hideRatings()
+        }
+    
         reviewHandler()
-      })
-
-      const options = {
-        attributes: true,
-        childList: true, 
-        subtree: true
-      }
-
-      observer.observe(reviewContainer, options)
     
+        const reviewContainer = document.querySelector('.wrapper-reviews')
+    
+          // Mutationobserver to observe changes to page
+          const observer = new MutationObserver(() => {
+            // console.log('mutation')
+            reviewHandler()
+          })
+    
+          const options = {
+            attributes: true,
+            childList: true, 
+            subtree: true
+          }
+    
+          observer.observe(reviewContainer, options)
+        })
+  // });
   })()
 })()
